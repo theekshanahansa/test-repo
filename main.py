98f6830b5526c53c0f4df1494db036d7345a162b
Theekshana <theekshanahansa@gmail.com>
2024-05-20 09:31:44 +0530

    change the name in person class

---
+++ main.py
@@ -0,0 +1,24 @@
+class Person:
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_age(self):
+        return self.__age
+
+    def set_age(self, age):
+        if age > 0:
+            self.__age = age
+
+
+person = Person("Theekshana", 30)
+
+print(person.get_name())
+person.set_age(31)
+print(person.get_age())
